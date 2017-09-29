d8100847283ba184728f084ac60e49adeed61a2e
Inho Choi <hasbegun@gmail.com>
2017-09-28 23:54:03 -0700

    Reformat

--- src/IrisAnalysis/GetROC.h
+++ src/IrisAnalysis/GetROC.h
@@ -10,78 +10,78 @@ using namespace std;
 class GetROC
 {
 public:
-
-	/**
-	* Get ROC scores
-	*
-	* @param colNum				Start from -1
-	* @param skip				Number of rows to skip (3 for our case)
-	* @param matchFName			Input matching score file name
-	* @param nonmatchFName		Input nonmatching score file name
-	* @param outputFName		Output ROC score file name
-	* @param pos				Valid \c pos values:
-	*							- \c 1: Left
-	*							- \c 2: Right		
-	* @param index   			Experiment tag number
-	*/
-	static void rocResult(int colNum, 
-						  int skip, 
-						  const char* matchFName, 
-						  const char* nonMatchFName, 
-						  const char* outputFName,
-						  int pos, 
-						  int index);
-  
-
-	/**
-	* Sort scores and rescale the length of nonmatching scores relavant to the matching length
-	*
-	* @param matchScores		match score vector
-	* @param nonmatchScores		nonmatch score vector
-	* @param newMatchScores		(OUT) sorted match score vector
-	* @param newNonmatchScores	(OUT) sorted/rescaled nonmatch score vector
-	*/
-	static void doNewOrderedScores(vector<float> matchScores,
-								   vector<float> nonMatchScores,
-								   vector<float>& newMatchScores,
-								   vector<float>& newNonMatchScores);
-	
-	/**
-	* Create ROC scores
-	*
-	* @param outputFName		Output ROC score file name
-	* @param matchScores		Input matching score vector
-	* @param nonmatchScores		Input nonmatching score vector	
-	* @param pos				Valid \c pos values:
-	*							- \c 1: Left
-	*							- \c 2: Right		
-	* @param index   			Experiment tag number
-	*/	
-	static void createROC(const char* outputFName, 
-					      vector<float> matchScores,
-						  vector<float> nonMatchScores,
-						  int pos, 
-						  int index);
-
-	/**
-	* Get reduced ROC scores
-	*
-	* @param colNum				Start from -1
-	* @param skip				Number of rows to skip (3 for our case)
-	* @param inputFName			Input file name with the full ROC score
-	* @param outputFName		(OUT) output file name with the reduced ROC score
-	* @param pos				Valid \c pos values:
-	*							- \c 1: Left
-	*							- \c 2: Right		
-	* @param index   			Experiment tag number
-	*/
-	static void reducedScores1(int colNum, 
-							   int skip,
-							   const char* inputFName,
-							   const char* outputFName,
-							   int pos, 
-							   int index);	
-  
+    
+    /**
+     * Get ROC scores
+     *
+     * @param colNum				Start from -1
+     * @param skip				Number of rows to skip (3 for our case)
+     * @param matchFName			Input matching score file name
+     * @param nonmatchFName		Input nonmatching score file name
+     * @param outputFName		Output ROC score file name
+     * @param pos				Valid \c pos values:
+     *							- \c 1: Left
+     *							- \c 2: Right		
+     * @param index   			Experiment tag number
+     */
+    static void rocResult(int colNum, 
+                          int skip, 
+                          const char* matchFName, 
+                          const char* nonMatchFName, 
+                          const char* outputFName,
+                          int pos, 
+                          int index);
+    
+    
+    /**
+     * Sort scores and rescale the length of nonmatching scores relavant to the matching length
+     *
+     * @param matchScores		match score vector
+     * @param nonmatchScores		nonmatch score vector
+     * @param newMatchScores		(OUT) sorted match score vector
+     * @param newNonmatchScores	(OUT) sorted/rescaled nonmatch score vector
+     */
+    static void doNewOrderedScores(vector<float> matchScores,
+                                   vector<float> nonMatchScores,
+                                   vector<float>& newMatchScores,
+                                   vector<float>& newNonMatchScores);
+    
+    /**
+     * Create ROC scores
+     *
+     * @param outputFName		Output ROC score file name
+     * @param matchScores		Input matching score vector
+     * @param nonmatchScores		Input nonmatching score vector	
+     * @param pos				Valid \c pos values:
+     *							- \c 1: Left
+     *							- \c 2: Right		
+     * @param index   			Experiment tag number
+     */	
+    static void createROC(const char* outputFName, 
+                          vector<float> matchScores,
+                          vector<float> nonMatchScores,
+                          int pos, 
+                          int index);
+    
+    /**
+     * Get reduced ROC scores
+     *
+     * @param colNum				Start from -1
+     * @param skip				Number of rows to skip (3 for our case)
+     * @param inputFName			Input file name with the full ROC score
+     * @param outputFName		(OUT) output file name with the reduced ROC score
+     * @param pos				Valid \c pos values:
+     *							- \c 1: Left
+     *							- \c 2: Right		
+     * @param index   			Experiment tag number
+     */
+    static void reducedScores1(int colNum, 
+                               int skip,
+                               const char* inputFName,
+                               const char* outputFName,
+                               int pos, 
+                               int index);	
+    
 };
 
 #endif // !__GET_ROC_H__
--- src/Masek/utility.cpp
+++ src/Masek/utility.cpp
@@ -3,20 +3,20 @@
 
 int Masek::fix(double x)
 {
-	int ret;	
-	ret = (int)x;	
-	return ret;
+    int ret;	
+    ret = (int)x;	
+    return ret;
 }
 
 
 int Masek::roundND(double x)
 {
-	int ret;
-	if (x >= 0.0)
-		ret = (int)(x+0.5);
-	else
-		ret = (int)(x-0.5);
-	return ret;
+    int ret;
+    if (x >= 0.0)
+        ret = (int)(x+0.5);
+    else
+        ret = (int)(x-0.5);
+    return ret;
 }
 
 
@@ -24,7 +24,7 @@ void Masek::printfilter(Masek::filter *mfilter, char *filename)
 {
     FILE *fid;
     int i, j;
-
+    
     fid = fopen(filename, "w");
     for (i = 0; i<mfilter->hsize[0]; i++)
         for (j = 0; j<mfilter->hsize[1]; j++)
@@ -35,14 +35,14 @@ void Masek::printfilter(Masek::filter *mfilter, char *filename)
                 fprintf(fid, "%d %d %f\n", i+1, j+1, mfilter->data[i*mfilter->hsize[1]+j]);
         }
     fclose(fid);
-
+    
 }
 
 void Masek::printimage(Masek::IMAGE *m, char *filename)
 {
     FILE *fid;
     int i, j;
-
+    
     fid = fopen(filename, "w");
     for (i = 0; i<m->hsize[0]; i++)
         for (j = 0; j<m->hsize[1]; j++)
@@ -50,5 +50,5 @@ void Masek::printimage(Masek::IMAGE *m, char *filename)
             fprintf(fid, "%d %d %d\n", i+1, j+1, m->data[i*m->hsize[1]+j]);
         }
     fclose(fid);
-
+    
 }
