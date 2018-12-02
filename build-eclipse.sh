2ada322882c1ff18f8d980d0559ebe1a489b3726
Inho Choi <hasbegun@gmail.com>
2018-12-02 02:54:28 -0800

    add eclipse

--- build-eclipse.sh
+++ build-eclipse.sh
@@ -2,7 +2,7 @@ export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 
 OpenCV_DIR=/usr/local/opencv/2.4.13.6
-BUILD_DIR=build-xcode
+BUILD_DIR=build-eclipse
 
 if [ -d $BUILD_DIR ]; then
     rm -rf ${BUILD_DIR}
@@ -14,5 +14,5 @@ cp ${OpenCV_DIR}/share/OpenCV/lbpcascades/lbpcascade_frontalface.xml .
 cp ${OpenCV_DIR}/share/OpenCV/haarcascades/haarcascade_eye*.xml .
 cp ${OpenCV_DIR}/share/OpenCV/haarcascades/haarcascade_front*.xml .
 
-cmake -G Xcode -D OpenCV_DIR=$OpenCV_DIR -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE -DCMAKE_INSTALL_PREFIX=/usr/local/alphablocks ..
-open BiometricLib.xcodeproj
+cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE -DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-j8 -D OpenCV_DIR=$OpenCV_DIR -DCMAKE_INSTALL_PREFIX=/usr/local/alphablocks ..
+
