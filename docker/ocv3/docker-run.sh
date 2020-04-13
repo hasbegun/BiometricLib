0e8457185e5321931af8f87dfe2a8427391338c9
Inho Choi <hasbegun@gmail.com>
2020-04-13 01:05:20 -0700

    Wrap qt5 compile

--- docker/ocv3/docker-run.sh
+++ docker/ocv3/docker-run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-echo "Host IP: $IP"
-
-/usr/X11/bin/xhost +$IP
-
 BIO_SRC=/Users/inho.choi/devel/alphablocks/projects/iris/BiometricLib
 BIO_DST=/home/developer/projects/Biometriclib
 FACE_RECOG_SRC=/Users/inho.choi/devel/alphablocks/projects/iris/face_recognition
@@ -14,13 +9,21 @@ IRIS_UX_DST=/home/developer/projects/IrisAnalysis
 
 CONTAINER_IP=192.168.0.50
 
+IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+echo "Host IP: $IP"
+
+# the following arg is optional. this is for vnc access.
+#  -v ${HOME}/.Xauthority:/home/developer/.Xauthority \
+#################################
+
+/usr/X11/bin/xhost +$IP
 exec docker run -it \
     -v  $BIO_SRC:$BIO_DST \
     -v  $FACE_RECOG_SRC:$FACE_RECOG_DST \
     -v  $IRIS_UX_SRC:$IRIS_UX_DST \
     -e DISPLAY=$IP:0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v ${HOME}/.Xauthority:/home/user/.Xauthority \
+    -v ${HOME}/.Xauthority:/home/developer/.Xauthority \
     --name iris-ocv3-qt5_dev \
     --ip $CONTAINER_IP \
     iris-ocv3-qt5:latest \
